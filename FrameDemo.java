f6f42fc2a1b130d6e626992fbda5b330e4e66565
pushparaj-1023 <pushparajd870@gmail.com>
2026-03-13 10:49:57 +0530

    ex.1

---
+++ FrameDemo.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+class FrameDemo
+{
+    public static void main(String args[])
+    {
+        Frame f = new Frame();
+
+        f.setTitle("Pushparaj - BCA");
+        f.setSize(500,400);
+        f.setVisible(true);
+    }
+}
